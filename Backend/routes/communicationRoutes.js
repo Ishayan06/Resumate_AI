02c42e972bad16bea53b87cc070b2ba45f4505bc
Ishayan kundu <ishayankundu6@gmail.com>
2026-05-13 15:41:34 +0530

    check

--- Backend/routes/communicationRoutes.js
+++ Backend/routes/communicationRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { updatePoints, getPoints } = require('../controllers/communicationController');
-const authMiddleware = require('../middleware/auth'); // same as your other routes
+const authMiddleware = require('../middleware/authMiddleware'); // same as your other routes
 
 router.post('/update-points', authMiddleware, updatePoints);
 router.get('/points', authMiddleware, getPoints);
