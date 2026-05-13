e02ee5b84e641a49c5d679bb4ef0e5cd835bf2e1
Ishayan kundu <ishayankundu6@gmail.com>
2026-05-14 01:18:57 +0530

    final code

--- Backend/server.js
+++ Backend/server.js
@@ -54,7 +54,7 @@ app.use('/api/dsa-plan',  dsaPlanRoutes);
 app.use('/api/session', sessionRoutes);
 
 
-cron.schedule('5 1 * * *', () => {
+cron.schedule('22 1 * * *', () => {
   sendDailyReminders().catch(console.error);
 }, {
   timezone: "Asia/Kolkata"
